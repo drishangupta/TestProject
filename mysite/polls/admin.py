dab05dc9394053beff3487eb6f7baf93a542250b
drishangupta <drishan2005@gmail.com>
2024-07-21 22:45:28 +0530

    admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,9 +1,14 @@
+
 from django.contrib import admin
 
-# Register your models here.
 from .models import Question
 
-class QuestionAdmin(admin.ModelAdmin):
-    fields=["pub_date","question_text"]
 
-admin.site.register(Question,QuestionAdmin)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
